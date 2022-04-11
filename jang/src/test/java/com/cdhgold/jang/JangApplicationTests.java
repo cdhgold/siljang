9ea37fdd46da8a90b1141c5833a5c96cf301fce4
cdhgold <cdhgold@naver.com>
2022-04-11 15:52:32 +0900

    4/11

---
+++ jang/src/test/java/com/cdhgold/jang/JangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdhgold.jang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
